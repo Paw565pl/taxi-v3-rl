312b9bc0b01c992f4f5d82ee3b1a34546f7357dd
Paw565pl <paw565pl@gmail.com>
2024-06-19 15:40:19 +0200

    ppo: create model testing function

---
+++ ppo/use_model.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+from stable_baselines3 import PPO
+
+
+def use_model(episodes: int):
+    env = gym.make("Taxi-v3", render_mode="human")
+    model = PPO.load("best_model", env=env)
+
+    vec_env = model.get_env()
+    observation = vec_env.reset()
+    for _ in range(episodes):
+        action, _states = model.predict(observation)
+        observation, rewards, done, info = vec_env.step(action)
+        vec_env.render("human")
+
+    vec_env.close()
+
+
+if __name__ == "__main__":
+    use_model(100)
